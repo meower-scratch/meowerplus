cb59611f38bafaa844eb038d52be5b62c3adf94b
William (Wgyt) <wgyt@usa.com>
2020-10-14 17:14:14 -0400

    Add files via upload

---
+++ backuporunuse/styleswitch.js
@@ -0,0 +1,15 @@
+//Saving theme to local storage (or saving something)
+var t = localStorage.getItem("meowerplus_theme");
+var s = document.querySelector("link");
+
+function theme(c) {
+  if (c == "dark") {
+    s.href = "dark.css";
+  } else {
+    s.href = "light.css";
+  }
+}
+
+function.auditlogs;
+getItem/<> 
+socket.io == (true)
